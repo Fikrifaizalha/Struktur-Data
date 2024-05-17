80c6475fe300fa51a6ddca8870514c63d6335870
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-17 13:27:04 +0700

    struktur data

--- Object Data.js
+++ Object Data.js
@@ -2,13 +2,13 @@ const fans = {
   namaSaya: "fikri",
   umurSaya: 27,
   hobiSaya: "bermain game",
-  pacarSaya: null,
+  "pacar saya": 2,
 };
 
 console.log(
   `hallo nama saya ${fans.namaSaya} umur saya ${fans.umurSaya} tahun`
 );
-console.log(`hobi saya ${fans.hobiSaya} dan pacar saya ${fans["pacarSaya"]}`);
+console.log(`hobi saya ${fans.hobiSaya} dan pacar saya ${fans["pacar saya"]}`);
 
 // Setelah mempelajari bagaimana membuat object dan menampilkan property di dalamnya, selanjutnya kita akan memodifikasi sebuah object.
 // Untuk mengubah nilai properti di dalam object kita gunakan assignment operator (=).
@@ -18,6 +18,7 @@ const united = {
   umurSaya: 27,
   hobiSaya: "bermain game",
   klubSaya: "Real Madrid",
+  "Hair Color": "blonde",
 };
 united.color = "red";
 united.klubSaya = "manchester United";
